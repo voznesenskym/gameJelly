977250290a57e44ea2bf5c827bb7a96b71b64d97
The Devil Incarnate (nee Adam) <adam.of.the.code@gmail.com>
2014-01-25 03:15:37 -0600

    Probably last commit before sleep.

---
+++ GameJam2D3D/Assets/Scripts/TotallyA2DCharacterController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TotallyA2DCharacterController : MonoBehaviour {
+
+	public Rigidbody2D playerRigidBody;
+
+	void Update()
+	{
+		if(Input.GetKey(KeyCode.I))
+			playerRigidBody.AddForce(new Vector2(0,10));
+		if(Input.GetKey(KeyCode.K))
+			playerRigidBody.AddForce(new Vector2(0,-10));
+		if(Input.GetKey(KeyCode.L))
+			playerRigidBody.AddForce(new Vector2(10,0));
+		if(Input.GetKey(KeyCode.J))
+			playerRigidBody.AddForce(new Vector2(-10,0));
+	}
+
+}
