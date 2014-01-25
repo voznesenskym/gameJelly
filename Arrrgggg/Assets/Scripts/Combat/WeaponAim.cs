83289cbc1f72c1a74dc734708f816e7ab761af16
r00b <msrubino@gmail.com>
2014-01-25 15:52:19 -0800

    pushing extension methods and temp sprites for shooting

---
+++ Arrrgggg/Assets/Scripts/Combat/WeaponAim.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponAim : MonoBehaviour {
+
+	#region Variables
+	public	PlatformerCharacter2D	controller;
+	public	Camera					mainCamera;
+	public	Transform				arm;
+
+	private	bool					_isFacingRight	=	true;
+	#endregion
+
+	#region Properties
+	#endregion
+
+	#region MonoBehaviour Methods
+	// Use this for initialization
+	void Start () {
+		if (!controller)	gameObject.GetComponentInParentRecursive<PlatformerCharacter2D>();
+		if (!camera)		mainCamera	=	Camera.main;
+		if (!arm)			arm			=	transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 	armForward	=	arm.right;
+		Vector3		toPoint		=	mainCamera.ScreenToWorldPoint(Input.mousePosition).XY() - arm.position;
+
+		if (!_isFacingRight) {
+			toPoint.y *= -1;
+		}
+		
+		float		zRot		=	Vector3.Angle(armForward, toPoint);
+					zRot		*=	Mathf.Sign(Vector3.Cross(armForward, toPoint).z);
+
+		arm.Rotate(0, 0, _isFacingRight ? zRot : zRot);
+		//arm.Rotate(0, 0, zRot);
+
+		if ((controller.IsFacingRight	&&	!_isFacingRight) ||
+		    (!controller.IsFacingRight	&&	_isFacingRight)) {
+			Flip();
+		}
+	}
+	#endregion
+
+	#region WeaponAim Methods
+	private void Flip() {
+		Vector3 scale	=	arm.localScale;
+				scale.x	*=	-1;
+				scale.y	*=	-1;
+		_isFacingRight	=	!_isFacingRight;
+		arm.localScale	=	scale;
+	}
+	#endregion
+}
