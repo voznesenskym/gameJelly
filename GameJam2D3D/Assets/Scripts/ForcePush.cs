8be0705c2a0487eb03697ffdf757a9864c74e655
The Devil Incarnate (nee Adam) <adam.of.the.code@gmail.com>
2014-01-25 00:41:39 -0600

    Added Adam's very own prototype in the making

---
+++ GameJam2D3D/Assets/Scripts/ForcePush.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForcePush : MonoBehaviour {
+
+	Camera Cam3D;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.E))
+		{
+			Ray ray = Cam3D.ScreenPointToRay(Input.mousePosition);
+			/*
+			RaycastHit hit;
+			if (Physics.Raycast(ray, out hit, 100))
+				Debug.DrawLine(ray.origin, hit.point);
+				*/
+		}
+	}
+}
