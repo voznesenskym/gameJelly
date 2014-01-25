bffbb82cb77f2baa0f2780dd38e22ecf8b5bd161
The Devil Incarnate (nee Adam) <adam.of.the.code@gmail.com>
2014-01-25 01:14:43 -0600

    added force push to 2d/3d game

--- GameJam2D3D/Assets/Scripts/ForcePush.cs
+++ GameJam2D3D/Assets/Scripts/ForcePush.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class ForcePush : MonoBehaviour {
 
-	Camera Cam3D;
+	public Camera Cam3D;
 
 	// Use this for initialization
 	void Start () {
@@ -14,12 +14,17 @@ public class ForcePush : MonoBehaviour {
 	void Update () {
 		if(Input.GetKey(KeyCode.E))
 		{
-			Ray ray = Cam3D.ScreenPointToRay(Input.mousePosition);
-			/*
+			Ray ray = Cam3D.ScreenPointToRay(new Vector3(Screen.width/2 + Screen.width/4, Screen.height/2, 0));
+
 			RaycastHit hit;
-			if (Physics.Raycast(ray, out hit, 100))
+			if (Physics.Raycast(ray, out hit, 10000))
+			{
 				Debug.DrawLine(ray.origin, hit.point);
-				*/
+
+				if(hit.rigidbody != null)
+					hit.rigidbody.AddForce(Cam3D.transform.forward * 30);
+			}
+
 		}
 	}
 }
