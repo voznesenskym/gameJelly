66056cb849c6bdf3497ba4c16a61bdffd8670bd9
Jason Roth <jmroth79@gmail.com>
2014-01-26 14:24:30 -0800

    cannonbll death

--- Arrrgggg/Assets/Scripts/Obsticles/Player3DTrigger.cs
+++ Arrrgggg/Assets/Scripts/Obsticles/Player3DTrigger.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class Player3DTrigger : MonoBehaviour {
 
 	public string cannonHitParticle = "CannonPlayerHit";
+	public Transform[] respawnPoints;
+	public GameObject player;
 
 	private Transform _transform;
 	private int particleCount;
@@ -24,7 +26,8 @@ public class Player3DTrigger : MonoBehaviour {
 			StartCoroutine(DestroyParticle(p));
 			if (other && other.gameObject) PhotonNetwork.Destroy(other.gameObject);
 			//Destroy(other.gameObject);
-
+			LockPlayerInPlace();
+			StartCoroutine(Respawn());
 		}
 	}
 
@@ -32,5 +35,36 @@ public class Player3DTrigger : MonoBehaviour {
 		yield return new WaitForSeconds(1.5f);
 		particleCount --;
 		if (ps && ps.gameObject) PhotonNetwork.Destroy(ps.gameObject);
+	}
+
+	private void LockPlayerInPlace() {
+		Debug.Log("lock");
+		player.GetComponent<Platformer2DUserControl>().enabled = false;
+		player.rigidbody2D.Sleep();
+	}
+	
+	private IEnumerator SplatPlayer() {
+		Debug.Log("Splat");
+		float t = 0;
+		float duration = 2;
+		while (t < duration) {
+			player.transform.position += new Vector3(0, 3, -6) * Time.deltaTime;
+			t += Time.deltaTime;
+			yield return null;
+		}
+	}
+	
+	private void LoseLife() {
+		LifeManager.Instance.RemoveLifeFrom(player.GetInstanceID());
+	}
+	
+	private IEnumerator Respawn() {
+		yield return StartCoroutine(SplatPlayer());
+		LoseLife();
+		int r = Random.Range(0, respawnPoints.Length);
+		yield return new WaitForSeconds(1.0f);
+		player.transform.position = respawnPoints[r].position;
+		player.GetComponent<Platformer2DUserControl>().enabled = true;
+		player.rigidbody2D.WakeUp();
 	}
 }
