29e104d2971428a40ee4006c954d0b629a2d0827
voznesenskym <voznesenskym@gmail.com>
2014-01-26 11:43:30 -0800

    how do i the game

--- Arrrgggg/Assets/Scripts/Networking/NetworkingManager.cs
+++ Arrrgggg/Assets/Scripts/Networking/NetworkingManager.cs
@@ -3,19 +3,19 @@ using System.Collections;
 using System.Linq;
 
 public class NetworkingManager : Photon.MonoBehaviour {
-	public string spawnObject = "2dSmoothingTest";
+	public string spawnObject = "2D Character";
 	public Transform spawnPosition;
 	public int connectionsAllowed = 4, portNumber = 5843;
-
+	
 	PlatformerCharacter2D character;
-
+	
 	float buttonWidth;
 	float buttonHeight;
-
+	
 	float buttonX;
 	float buttonY;
 	GameObject myPlayerGo;
-
+	
 	private string gameName;
 	private bool refreshing;
 	private RoomInfo[] roomInfo;
@@ -25,30 +25,30 @@ public class NetworkingManager : Photon.MonoBehaviour {
 	private Vector3 spawnPositionPoint2;
 	private bool connected;
 	private GameObject playerObject;
-
+	
 	// Use this for initialization
 	void Start () {
 		buttonX = Screen.width * .01f;
 		buttonY = Screen.height * .01f;
-
+		
 		buttonWidth = Screen.width * .1f;
 		buttonHeight = Screen.width * .1f;
-
+		
 		gameName = "Blind Pirate Fight";
-
+		
 		spawnPositionPoint0 = spawnPosition.position;
 		spawnPositionPoint1 = new Vector3 (spawnPosition.position.x + 5.0f, spawnPosition.position.y, spawnPosition.position.z);
 		spawnPositionPoint2 = new Vector3 (spawnPosition.position.x + 10.0f, spawnPosition.position.y, spawnPosition.position.z);
-
+		
 		PhotonNetwork.ConnectUsingSettings("v1.0");
 	}
-
+	
 	// Update is called once per frame
 	void OnGUI (){
 		enableUI ();
 	}
 	void enableUI(){
-
+		
 		if (!PhotonNetwork.isNonMasterClientInRoom && !PhotonNetwork.isMasterClient){
 			if (GUI.Button (new Rect (buttonX, buttonY, buttonWidth, buttonHeight), "Start Server")) {
 				Debug.Log ("Starting Server");
@@ -60,50 +60,50 @@ public class NetworkingManager : Photon.MonoBehaviour {
 			}
 			if (hostDataExists) {
 				for (int i = 0; i < roomInfo.Length; i++) {
-					if (GUI.Button(new Rect(buttonX + (80 * i), buttonY * 2 + buttonHeight * 2 , buttonWidth, buttonHeight), roomInfo[i].name)){
+					if (GUI.Button(new Rect(buttonX, buttonY * 2 + buttonHeight * 2 , buttonWidth, buttonHeight), roomInfo[i].name)){
 						PhotonNetwork.JoinRoom(roomInfo[i].name);
 					}				
 				}		
 			}
 		}
-
+		
 		if (connected) {
 			if (GUI.Button (new Rect (buttonX, buttonY, buttonWidth, buttonHeight), "Disconnect")) {
 				Debug.Log ("disconnecting");
 				connected = false;
-
+				
 				if (PhotonNetwork.isMasterClient) {
 					// TODO on server close clean up everyone
 				}
-
+				
 				PhotonNetwork.LeaveRoom();
 				Cleanup();
 			};
 		}
-
+		
 	}
 	void startServer(){
 		string nm = gameName + new System.DateTime().DateTimeToUnixTimestamp();
 		PhotonNetwork.CreateRoom(nm);
 	}
-
+	
 	void OnJoinedRoom () {
 		spawnPlayer ();
-
+		
 	}
-
+	
 	void OnDisconnectedFromServer (){
 		enableUI();
 	}
-
+	
 	void showDisconnectButton(){
 	}
-
+	
 	void refreshHostList() {
 		roomInfo = PhotonNetwork.GetRoomList();
 		hostDataExists = true;
 	}
-
+	
 	void spawnPlayer () {
 		connected = true;
 		showDisconnectButton ();
@@ -118,17 +118,18 @@ public class NetworkingManager : Photon.MonoBehaviour {
 			myPlayerGo = PhotonNetwork.Instantiate (spawnObject, spawnPositionPoint2, Quaternion.identity, 0);
 			turnStuffOnAtInstantiationOfPlayer();
 		}
-
-		StartCoroutine(RegisterLives());
+		
+		
 	}
-
+	
 	void turnStuffOnAtInstantiationOfPlayer(){
 		((MonoBehaviour)myPlayerGo.GetComponent ("PlatformerCharacter2D")).enabled = true;
 		((MonoBehaviour)myPlayerGo.GetComponent ("Platformer2DUserControl")).enabled = true;
 		myPlayerGo.GetComponent<Animator>().enabled = true;
-
+		myPlayerGo.GetComponent<CircleCollider2D>().enabled = true;
+		myPlayerGo.GetComponent<SpriteRenderer>().enabled = true;
 	}
-
+	
 	void Cleanup() {
 		//GameObject myPlayer = GameObject.FindGameObjectsWithTag("Player").Where(p => p.GetComponent<PhotonView>().isMine == true).FirstOrDefault();
 		GameObject[] myPlayer = GameObject.FindGameObjectsWithTag ("Player");
@@ -136,10 +137,5 @@ public class NetworkingManager : Photon.MonoBehaviour {
 		foreach (GameObject player in myPlayer) {
 			Destroy(player);
 		}
-	}
-
-	IEnumerator RegisterLives() {
-		yield return null;
-		if (LifeManager.Instance != null) LifeManager.Instance.RegisterPlayerLives();
 	}
 }
