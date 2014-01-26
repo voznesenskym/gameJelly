64bed8ef89c220b13968ec6c172d3003898b397c
r00b <msrubino@gmail.com>
2014-01-26 09:18:55 -0800

    test

---
+++ Arrrgggg/Assets/Scripts/Networking/SyncPosition.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SyncPosition : MonoBehaviour {
+
+	void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+	{
+		if (stream.isWriting)
+		{
+			//We own this player: send the others our data
+			stream.SendNext(transform.position);
+		}
+		else
+		{
+			//Network player, receive data
+			transform.position = (Vector3)stream.ReceiveNext();
+		}
+	}
+}
