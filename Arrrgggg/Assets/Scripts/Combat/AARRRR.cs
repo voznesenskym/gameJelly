1f30dcdf25e956749b0c44bf17c447c51e617c09
r00b <msrubino@gmail.com>
2014-01-25 19:45:17 -0800

    added networking for bullets

--- Arrrgggg/Assets/Scripts/Combat/AARRRR.cs
+++ Arrrgggg/Assets/Scripts/Combat/AARRRR.cs
@@ -1,28 +1,42 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(NetworkView))]
 public class AARRRR : MonoBehaviour {
-
+	
 	public	float	lifetime		=	4f;
 	public	float	speed			=	1f;
-
-	private	float	_lifetimeTimer	=	0f;
-
+	
+	private	float		_lifetimeTimer	=	0f;
+	private NetworkView	_view;
+	
+	void Awake() {
+		_view	=	GetComponent<NetworkView>();	
+	}
+	
 	// Update is called once per frame
 	void Update () {
 		UpdateLifetime();
 		Move();
 	}
-
+	
 	void UpdateLifetime() {
 		_lifetimeTimer += Time.fixedDeltaTime;
-
+		
 		if (_lifetimeTimer > lifetime) {
 			Destroy(gameObject);
 		}
 	}
-
+	
 	void Move() {
 		transform.Translate(transform.right * speed * Time.fixedDeltaTime, Space.World);
+	}
+	
+	void OnCollisionEnter2D(Collision2D collision) {
+		GameObject player	=	collision.gameObject;
+		
+		if (player.CompareTag("Player") && player.GetComponent<NetworkView>().isMine && _view.isMine) {
+			// TODO lights out
+		}
 	}
 }
